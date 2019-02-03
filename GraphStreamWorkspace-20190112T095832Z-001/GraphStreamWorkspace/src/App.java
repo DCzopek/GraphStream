aaa0b0606c33fa306652f52e59e0d869d4a76cde
dczopek <czopekdavid@gmail.com>
2019-02-03 13:34:10 +0100

    final working project

--- GraphStreamWorkspace-20190112T095832Z-001/GraphStreamWorkspace/src/App.java
+++ GraphStreamWorkspace-20190112T095832Z-001/GraphStreamWorkspace/src/App.java
@@ -3,70 +3,233 @@ import org.graphstream.graph.implementations.SingleGraph;
 import org.graphstream.ui.view.Viewer;
 
 
-
 import javax.swing.*;
+import javax.swing.text.html.HTMLDocument;
 import java.awt.*;
 import java.io.Console;
-import java.util.LinkedList;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.*;
 import java.util.List;
-import java.util.Random;
+import java.util.Timer;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 
 public class App {
 
+    private static void generateOutput(Graph graph) {
+        //Generowanie wyjścia do pliku txt
 
-    public static void displayGraph(int vertexes, double probability){
+        Date date = new Date();
 
-        String auxName;
+        System.out.println("-----------------------------------------");
+        System.out.println("Autg. Sem.5 | Wyszukiwanie cykli\n");
+        System.out.println("Zespol: D.Czopek / T.Dziubinski / P.Zajac\n");
+        System.out.println("Data wygenerowania: " + date + "\n");
+        System.out.println("Ilość wierzchołków: " + graph.getNodeCount());
+        System.out.println("Ilość krawędzi: " + graph.getEdgeCount());
+        System.out.println();
 
-        Graph graph = new SingleGraph("Tutor 1");
+        Collection<Node> nodesList = graph.getNodeSet();
 
+        for (Node node : nodesList) {
+            System.out.print("Sasiedzi wierzcholka \"" + node.getId() + "\" : ");
+            Iterator itr = node.getNeighborNodeIterator();
 
-        List<Node> nodes = new LinkedList<Node>();
-        List<Edge> edges = new LinkedList<Edge>();
-
-
-        for (int i = 0 ; i < vertexes ; i++)
-        {
-            auxName = Integer.toString(i);
-             graph.addNode(auxName);
-             nodes.add(graph.getNode(auxName));
-
-            //System.out.println(graph.getNode(auxName).getIndex());
-
-            if(graph.getNode(auxName).getIndex() % 2 == 0 )
-                            graph.getNode(auxName).addAttribute("ui.style", "fill-color: red;size: 20px;  stroke-mode: plain; stroke-color: #333;");
-                     else
-                            graph.getNode(auxName).addAttribute("ui.style", "fill-color: blue; size: 20px; stroke-mode: plain; stroke-color: #333; ") ;
-
+            while (itr.hasNext()) System.out.print(itr.next() + "; ");
+            System.out.println();
         }
 
-        for(int j = 0 ; j < vertexes ;j++ )
-        {
+        System.out.println("\nMacierz sasiedztwa: \n");
+        System.out.print("   ");
+
+        for (Node node : nodesList) System.out.print(node.getId() + "  ");
+        System.out.println();
+        System.out.print("  ");
+        for (Node node : nodesList) System.out.print("---");
+        System.out.println();
+
+        for (Node node : nodesList) {
+            System.out.print(node + " |");
+            for (Node checkedNode : nodesList) {
+                if (node == checkedNode) System.out.print("0, ");
+                else {
+                    if (node.hasEdgeBetween(checkedNode)) System.out.print("1, ");
+                    else System.out.print("0, ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+
+    private static List<String> getNodesNamesList(int vertexes) {
+
+        String auxName;
+        Integer alphabetIterator = 0;
+        List<String> nodesNames = new ArrayList<>();
+
+        for (int i = 0; i < vertexes; i++) {
+            auxName = "";
+            if (alphabetIterator == 0)
+                auxName += (char) (i % 26 + 65);
+            else {
+                auxName += ((char) (i % 26 + 65)) + (alphabetIterator.toString());
+            }
+
+            nodesNames.add(auxName);
+
+            if (i % 26 == 25) alphabetIterator++;
+        }
+
+        return nodesNames;
+    }
+
+    private static Graph generateSingleGraph(int vertexes, double probability) {
+
+        Graph graph = new SingleGraph("Tutor 1");
+        List<String> nodeNames = getNodesNamesList(vertexes);
+
+        for (int i = 0; i < vertexes; i++) {
+            graph.addNode(nodeNames.get(i));
+            graph.getNode(nodeNames.get(i)).setAttribute("ui.label", nodeNames.get(i));
+        }
+
+        for (int j = 0; j < vertexes; j++) {
             Random rand = new Random();
 
             for (int k = 0; k < vertexes; k++) {
 
-                if((rand.nextDouble() < probability)&&(j<k))
-                {
-                    auxName = Integer.toString(j)+Integer.toString(k);
-                    graph.addEdge(auxName,Integer.toString(j),Integer.toString(k));
-                    edges.add(graph.getEdge(auxName));
-
-                    graph.getEdge(auxName).setAttribute("ui.style" , "shape: blob; size: 5  px; arrow-shape: none; ");
-
+                if ((rand.nextDouble() < probability) && (j < k)) {
+                    graph.addEdge(nodeNames.get(j) + nodeNames.get(k), nodeNames.get(j), nodeNames.get(k));
                 }
             }
 
         }
 
-//        for (Node node : nodes)
-//        {
-//            System.out.println(node.getId());
-//        }
+        graph.addAttribute("ui.stylesheet"," graph { fill-color: black; } node { size: 15px; fill-color: #44F; stroke-mode: plain; stroke-width: 2px; stroke-color: #CCF; shadow-mode: gradient-radial; shadow-width: 3px; shadow-color: #EEF, #000; shadow-offset: 0px; text-alignment: at-right; text-padding: 3px, 2px; text-background-mode: rounded-box; text-background-color: #EB2; text-color: #222;} edge { fill-color: #CCF; size: 2px;}");
+        return graph;
+    }
 
-       Viewer viewer = graph.display();
+    static void displaySimpleGraph(int vertexes, double probability) {
+
+        Graph graph = generateSingleGraph(vertexes, probability);
+        Viewer viewer = graph.display();
         viewer.setCloseFramePolicy(Viewer.CloseFramePolicy.CLOSE_VIEWER);
+        generateOutput(graph);
+    }
+
+    static void displayGraphWithCycles(int vertexes, double probability, long highlightingTime) throws InterruptedException {
+
+        Graph graph = generateSingleGraph(vertexes, probability);
+        List<String> nodesNames = getNodesNamesList(vertexes);
+        List<List<String>> cycleNodes = new ArrayList<>();
+
+        String currentNodeId;
+
+
+        for (String node : nodesNames) {
+
+            currentNodeId = node;
+
+            List<String> currentNodeNeighbours = new ArrayList<>();
+            Iterator itr = graph.getNode(currentNodeId).getNeighborNodeIterator();
+
+            while (itr.hasNext()) {
+                currentNodeNeighbours.add(itr.next().toString());
+            }
+
+
+            for (String neighbourNode : currentNodeNeighbours) {
+                Iterator nItr = graph.getNode(neighbourNode.toString()).getNeighborNodeIterator();
+
+                //Dodawanie listy nodów z cyklu do listy cyklów
+
+                while (nItr.hasNext()) {
+                    Object nItrElement = nItr.next();
+                    if (currentNodeNeighbours.contains(nItrElement.toString())) {
+
+                        //dodawanie nodów do pojedynczego cyklu
+
+                        List<String> cycle = new ArrayList<>();
+                        cycle.add(nItrElement.toString());
+                        cycle.add(currentNodeId);
+                        cycle.add(neighbourNode);
+
+                        //sortowanie w celu sprawdzenia czy dany cykl jest już w liście cykli
+
+                        var sortedList = cycle.stream().sorted().collect(Collectors.toList());
+                        if (!(cycleNodes.contains(sortedList)))
+                            cycleNodes.add(sortedList);
+                    }
+                }
+
+
+            }
+
+
+        }
+
+        generateOutput(graph);
+        System.out.println("\nLista wierzcholkow cykli trojelementowych: \n");
+        for (List<String> cycle : cycleNodes) {
+            System.out.print(cycleNodes.indexOf(cycle) + ".: ( ");
+            for (String node : cycle) {
+                System.out.print(node + ", ");
+            }
+            System.out.println(")");
+        }
+        Viewer viewer = graph.display();
+        viewer.setCloseFramePolicy(Viewer.CloseFramePolicy.CLOSE_VIEWER);
+
+        //Tworzenie listy krawędzi w cyklach
+
+        new Thread(() ->
+
+
+        {
+
+            while (true) {
+                for (List<String> cycle : cycleNodes) {
+
+                    List<Edge> edgesInCycle = new ArrayList<>();
+
+                    edgesInCycle.add(graph.getNode(cycle.get(0)).getEdgeBetween(cycle.get(1)));
+                    edgesInCycle.add(graph.getNode(cycle.get(0)).getEdgeBetween(cycle.get(2)));
+                    edgesInCycle.add(graph.getNode(cycle.get(1)).getEdgeBetween(cycle.get(2)));
+
+                    for (Edge edge : edgesInCycle) {
+                        edge.addAttribute("ui.style", "fill-color: red; size: 4px;");
+                    }
+
+                    for (String node : cycle) {
+
+                        graph.getNode(node).addAttribute("ui.style", "fill-color: red;");
+                    }
+
+
+                    try {
+                        Thread.sleep(highlightingTime * 1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    for (Edge edge : edgesInCycle) {
+                        edge.addAttribute("ui.style", "fill-color: #CCF; size: 2px;");
+                    }
+
+                    for (String node : cycle) {
+
+                        graph.getNode(node).addAttribute("ui.style", "fill-color: #44F;");
+                    }
+
+                }
+            }
+        }
+        ).start();
 
     }
 
@@ -75,20 +238,27 @@ public class App {
         System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
         UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 
-
-
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
 
                 MainFrame mainFrame = new MainFrame();
 
-                mainFrame.setSize(400,400);
+                //Zmiana Streamu wyjściowego na txt
+
+                PrintStream out = null;
+                try {
+                    out = new PrintStream(new FileOutputStream("output.txt", true));
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                System.setOut(out);
+
+                mainFrame.setSize(400, 400);
                 mainFrame.setVisible(true);
 
             }
         });
-
 
 
     }
--- GraphStreamWorkspace-20190112T095832Z-001/GraphStreamWorkspace/src/MainFrame.java
+++ GraphStreamWorkspace-20190112T095832Z-001/GraphStreamWorkspace/src/MainFrame.java
@@ -7,23 +7,30 @@ public class MainFrame extends JFrame {
     private JPanel jPanel;
     private JButton generateButton;
     private JTextField vertexField;
-    private JTextField edgesField;
+    private JTextField probabilityField;
     private JPanel SettingsPanel;
-    private JTextField vertexesInCykleField;
     private JButton generateAndSearchCykleButton;
+    private JTextField highlightingTime;
 
     public MainFrame()
     {
         add(jPanel);
         setTitle("Graphs");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Pola wypełniane automatycznie przy uruchomieniu - dla ułatwienia testowania
+        //-----------------------------------------------
+        vertexField.setText("10");
+        probabilityField.setText("0.5");
+        //------------------------------------------------
+
         generateButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
 
                 try {
-                    if(Double.parseDouble(edgesField.getText()) > 1)
+                    if(Double.parseDouble(probabilityField.getText()) > 1)
                     {
                         JOptionPane.showMessageDialog(jPanel, "Prawdopodobieństwo powinno być z przedziału 0-1 oraz być zapisane w notacji z kropką (np. 0.5)");
 
@@ -33,7 +40,7 @@ public class MainFrame extends JFrame {
 
                     }
                     else
-                App.displayGraph(Integer.parseInt(vertexField.getText()),Double.parseDouble(edgesField.getText()));
+                App.displaySimpleGraph(Integer.parseInt(vertexField.getText()),Double.parseDouble(probabilityField.getText()));
                 }
                 catch (NumberFormatException exc){
                     JOptionPane.showMessageDialog(jPanel, "Ilość wierzchołków powinna być liczbą całkowitą < 120.\nPrawdopodobieństwo powinno być z przedziału 0 - 1 oraz być zapisane w notacji z kropką (np. 0.5)");
@@ -41,12 +48,13 @@ public class MainFrame extends JFrame {
 
             }
         });
+        generateAndSearchCykleButton.setText("<html><center>"+"Generuj graf i wyszukaj"+"<br>"+"cykle trójelementowe"+"</center></html>");
         generateAndSearchCykleButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
 
                 try {
-                    if(Double.parseDouble(edgesField.getText()) > 1)
+                    if(Double.parseDouble(probabilityField.getText()) > 1)
                     {
                         JOptionPane.showMessageDialog(jPanel, "Prawdopodobieństwo powinno być z przedziału 0-1 oraz być zapisane w notacji z kropką (np. 0.5)");
 
@@ -55,17 +63,12 @@ public class MainFrame extends JFrame {
                         JOptionPane.showMessageDialog(jPanel, "Ilość wierzchołków powinna być liczbą całkowitą < 120");
 
                     }
-                    else if(Integer.parseInt(vertexesInCykleField.getText()) > Integer.parseInt(vertexField.getText()) ) {
-                        JOptionPane.showMessageDialog(jPanel, "Ilość wierzchołków w cyklu nie może być większa niż ilość wierzchołków grafu");
-                    }
-                    else if(Integer.parseInt(vertexesInCykleField.getText()) < 3 || Integer.parseInt(vertexesInCykleField.getText()) > 6){
-                        JOptionPane.showMessageDialog(jPanel, "Ilość wierzchołków w cyklu musi zawierać się w przedziale: 3 - 6");
-                    }
-                    else
-                        App.displayGraph(Integer.parseInt(vertexField.getText()),Double.parseDouble(edgesField.getText()));
+                        App.displayGraphWithCycles(Integer.parseInt(vertexField.getText()),Double.parseDouble(probabilityField.getText()), Long.parseLong(highlightingTime.getText()));
                 }
                 catch (NumberFormatException exc){
-                    JOptionPane.showMessageDialog(jPanel, "Ilość wierzchołków powinna być liczbą całkowitą < 120.\nPrawdopodobieństwo powinno być z przedziału 0 - 1 oraz być zapisane w notacji z kropką (np. 0.5)");
+                    JOptionPane.showMessageDialog(jPanel, "Ilość wierzchołków powinna być liczbą całkowitą < 120.\nPrawdopodobieństwo powinno być z przedziału 0 - 1 oraz być zapisane w notacji z kropką (np. 0.5)\nDla wyświetlenia cykli grafu podaj czas podświetlania.");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
 
             }
